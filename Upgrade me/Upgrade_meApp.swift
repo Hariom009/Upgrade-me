83a0b07c813698c1ff07d68c753dd980bded6819
Hariom009 <hariomconnects03@gmail.com>
2025-04-16 19:01:39 +0530

    Initial Commit

---
+++ Upgrade me/Upgrade_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Upgrade_meApp.swift
+//  Upgrade me
+//
+//  Created by Hari's Mac on 16.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Upgrade_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Upgrade meTests/Upgrade_meTests.swift
@@ -0,0 +1,17 @@
+//
+//  Upgrade_meTests.swift
+//  Upgrade meTests
+//
+//  Created by Hari's Mac on 16.04.2025.
+//
+
+import Testing
+@testable import Upgrade_me
+
+struct Upgrade_meTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
